a591a3b4fabd0334b71a387700b6c768acb71aff
Joao Ricardo <jricardollanque@gmail.com>
2024-02-24 19:08:58 +0000

    Getting first and second loop done

--- Assets/JoaoRicardo/JaoScripts/PortalManager.cs
+++ Assets/JoaoRicardo/JaoScripts/PortalManager.cs
@@ -8,10 +8,13 @@ public class PortalManager : MonoBehaviour
     public Transform nextFloor;
     public GameObject previousFloor;
 
+    private Quaternion rot = Quaternion.Euler(0f, 180f, 0f);
+
 
     private void OnTriggerEnter(Collider other)
     {
         other.transform.position = nextFloor.transform.position;
+        other.transform.rotation = rot;
         Destroy(previousFloor);
 
     }
