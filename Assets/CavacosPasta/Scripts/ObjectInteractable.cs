4fd35e92cc41e9896a543052918a96be96a2b6ef
João Cavacos <cavacosdoesgaming@gmail.com>
2024-03-05 19:03:40 +0000

    (FIX) New door open interaction & plate 205 interaction fix

--- Assets/CavacosPasta/Scripts/ObjectInteractable.cs
+++ Assets/CavacosPasta/Scripts/ObjectInteractable.cs
@@ -7,17 +7,20 @@ using UnityEngine.Events;
 public class ObjectInteractable : MonoBehaviour, IInteractable
 {
     public string textToDisplay;
-
-    public AudioClip paper;
-
+    public AudioClip interactSound;
     public UnityEvent OnEndofInteract;
     
     public void Interact()
     {
         UIController.Instance.ClearText();
-        SoundManager.Instance.PlayAudio(paper);
         UIController.Instance.SetDescriptionText(textToDisplay, 2f);
+        if(interactSound != null) SoundManager.Instance.PlayAudio(interactSound);
         OnEndofInteract?.Invoke();
+    }
+
+    public void DestroyInteraction()
+    {
+        Destroy(this);
     }
     
 }
